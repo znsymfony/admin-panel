18d7447cc67df886bff68ee38ed1e633baa868f7
Vitaliy <theyamshikov@yandex.ru>
2021-08-06 09:34:03 +0600

    upd

--- src/Symfony4/Admin/views/layouts/admin/block/main-sidebar.php
+++ src/Symfony4/Admin/views/layouts/admin/block/main-sidebar.php
@@ -17,7 +17,7 @@ use ZnLib\Web\Widgets\AdminLte3\Sidebar\SidebarWidget;
 <a href="<?= Url::to('/admin') ?>" class="brand-link">
     <img src="/dist/img/icon/cloud-network.png" alt="Logo" class="brand-image img-circle <!--elevation-3-->"
          style="opacity: .8">
-    <!--<span class="brand-text font-weight-light">Brand</span>-->
+    <span class="brand-text font-weight-light">Admin panel</span>
 </a>
 
 <!-- Sidebar -->
